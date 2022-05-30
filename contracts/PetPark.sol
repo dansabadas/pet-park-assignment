3f8aa4bf42fa1701b5fa945a623064f80826cf4a
Dan Sabadis <dsabadas@strategikonpharma.com>
2022-05-30 18:39:48 +0300

    added

--- contracts/PetPark.sol
+++ contracts/PetPark.sol
@@ -3,5 +3,14 @@ pragma solidity ^0.8.0;
 
 
 contract PetPark {
+    enum AnimalType{ None, Fish, Cat, Dof, Rabbit, Parrot }
 
+    address owner;
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function add(AnimalType _animalType, uint _age) public view {
+        require(msg.sender == owner, "Not an owner");
+    }
 }
