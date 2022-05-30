eba56d8c917bc96abe2b971951c67227a1126131
Dan Sabadis <dsabadas@strategikonpharma.com>
2022-05-30 19:11:31 +0300

    borrow

--- contracts/PetPark.sol
+++ contracts/PetPark.sol
@@ -4,13 +4,24 @@ pragma solidity ^0.8.0;
 
 contract PetPark {
     enum AnimalType{ None, Fish, Cat, Dof, Rabbit, Parrot }
+    enum Gender{ Male, Female }
+
+    event Added(AnimalType animalType, uint _age);
 
     address owner;
     constructor() {
         owner = msg.sender;
     }
 
-    function add(AnimalType _animalType, uint _age) public view {
+    function add(AnimalType _animalType, uint _age) public {
         require(msg.sender == owner, "Not an owner");
+        require(_animalType != AnimalType.None, "Invalid animal");
+
+
+        emit Added(_animalType, _age);
+    }
+
+    function borrow(uint _age, Gender _gender, AnimalType _animalType) public pure {
+        require(_age != 0, "Invalid Age");
     }
 }
